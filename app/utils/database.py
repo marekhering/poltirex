36ad923071b8ee76e59ab99b37177b818c51ea78
Marek <marek.hering99@gmail.com>
2021-11-28 11:00:00 +0100

    set database connection using psycopg2

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,12 +1,23 @@
+from urllib.parse import urlparse
 import psycopg2
+import os
 
 
 def set_connection():
+    result = urlparse(os.getenv("DATABASE_URL"))
+    username = result.username
+    password = result.password
+    database = result.path[1:]
+    hostname = result.hostname
+    port = result.port
+
+    print(result, database, port, hostname, username, password)
+
     return psycopg2.connect(
-        user='postgres',
-        password='postgres',
-        host='127.0.0.1',
-        port='5432',
-        database='postgres'
+        user=username,
+        password=password,
+        host=hostname,
+        port=port,
+        database=database
     )
 
